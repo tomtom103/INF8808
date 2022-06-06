6d908f5a141c903249582f9ab714c0644e5cf07e
Thomas Caron <thomas.caron@polymtl.ca>
2022-06-06 19:17:42 -0400

    Revert "Fixed apostrophe"
    This reverts commit 52b0c6759a174c38c631415603a3bdd5c49e2c5c.

--- TP5/src/scripts/preprocess.js
+++ TP5/src/scripts/preprocess.js
@@ -1,8 +1,8 @@
 const TITLES = {
   '1. Noyau villageois': 'Noyau villageois',
-  '2. Rue commerciale de quartier, d\'ambiance ou de destination': 'Rue commerciale de quartier, d\'ambiance ou de destination',
+  '2. Rue commerciale de quartier, d’ambiance ou de destination': 'Rue commerciale de quartier, d’ambiance ou de destination',
   '3. Rue transversale à une rue commerciale': 'Rue transversale à une rue commerciale',
-  '4. Rue bordant un bâtiment public ou institutionnel  (tels qu\'une école primaire ou secondaire, un cégep ou une université, une station de métro, un musée, théâtre, marché public, une église, etc.)': 'Rue bordant un bâtiment public ou institutionnel',
+  '4. Rue bordant un bâtiment public ou institutionnel  (tels qu’une école primaire ou secondaire, un cégep ou une université, une station de métro, un musée, théâtre, marché public, une église, etc.)': 'Rue bordant un bâtiment public ou institutionnel',
   '5. Rue en bordure ou entre deux parcs ou place publique': 'Rue en bordure ou entre deux parcs ou place publique',
   '6. Rue entre un parc et un bâtiment public ou institutionnel': 'Rue entre un parc et un bâtiment public ou institutionnel',
   '7. Passage entre rues résidentielles': 'Passage entre rues résidentielles'
